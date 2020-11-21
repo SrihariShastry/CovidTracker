0b9777c8299fce4bb4ac50e0f20f1b87dec56d33
SrihariShastry <sriharishastry.93@gmail.com>
2020-11-20 22:44:52 -0700

    Adding State Lists

--- src/main/java/com/example/CovidTracker/Controller/HomeController.java
+++ src/main/java/com/example/CovidTracker/Controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.example.CovidTracker.Controller;
 
+import com.example.CovidTracker.services.CovidDataService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public String home(){
-        return "home";
+    @Autowired
+    CovidDataService service;
 
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("stateList", service.getStateList());
+        return "home";
     }
 }
--- src/main/java/com/example/CovidTracker/services/CovidDataService.java
+++ src/main/java/com/example/CovidTracker/services/CovidDataService.java
@@ -6,24 +6,39 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
 import java.net.URL;
+import java.nio.file.Paths;
+import java.util.HashMap;
 
 @Service
 public class CovidDataService {
 
     private final String VIRUS_DATA_URL="https://api.covidtracking.com/v1/states/ca/current.json";
+    private CurrentStats currentStat;
+    private HashMap<String,String> stateList;
+
+    public CurrentStats getCurrentStat() {
+        return currentStat;
+    }
+    public HashMap<String,String> getStateList(){
+        return stateList;
+    }
 
     //execute after creating the instance of this service
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchData(){
+        stateList = new HashMap<>();
 
         try {
             ObjectMapper mapper = new ObjectMapper();
-            CurrentStats currentStat= mapper.readValue(new URL(VIRUS_DATA_URL),CurrentStats.class);
-            System.out.println("hello");
+            currentStat= mapper.readValue(new URL(VIRUS_DATA_URL),CurrentStats.class);
+            stateList = mapper.readValue(new File("src/main/resources/state-list.json"),HashMap.class);
+//            System.out.println("hello");
         }catch (Exception e){
             e.printStackTrace();
+
         }
     }
 }
