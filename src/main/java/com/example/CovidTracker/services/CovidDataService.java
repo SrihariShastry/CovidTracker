9b1d4c26cc94fbc3752f6584b06eb1ca1708d10b
SrihariShastry <sriharishastry.93@gmail.com>
2020-11-22 23:07:01 -0700

    Displaying data based on selected state

--- src/main/java/com/example/CovidTracker/services/CovidDataService.java
+++ src/main/java/com/example/CovidTracker/services/CovidDataService.java
@@ -13,28 +13,26 @@ import java.util.HashMap;
 @Service
 public class CovidDataService {
 
-    private final String VIRUS_DATA_URL="https://api.covidtracking.com/v1/states/ca/current.json";
     private CurrentStats currentStat;
-    private HashMap<String,String> stateList;
+    private String URL;
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
 
     public CurrentStats getCurrentStat() {
         return currentStat;
     }
-    public HashMap<String,String> getStateList(){
-        return stateList;
-    }
 
     //execute after creating the instance of this service
-    @PostConstruct
-    @Scheduled(cron = "* * 1 * * *")
-    public void fetchData(){
-        stateList = new HashMap<>();
+//    @PostConstruct
+//    @Scheduled(cron = "* * 1 * * *")
+    public void fetchData(String url){
+//        stateList = new HashMap<>();
 
         try {
             ObjectMapper mapper = new ObjectMapper();
-            currentStat= mapper.readValue(new URL(VIRUS_DATA_URL),CurrentStats.class);
-            stateList = mapper.readValue(new File("src/main/resources/state-list.json"),HashMap.class);
-//            System.out.println("hello");
+            this.currentStat= mapper.readValue(new URL(url),CurrentStats.class);
         }catch (Exception e){
             e.printStackTrace();
 
---
+++ src/main/java/com/example/CovidTracker/services/StateListService.java
@@ -0,0 +1,33 @@
+package com.example.CovidTracker.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.util.HashMap;
+
+@Service
+public class StateListService {
+
+    private final String PATH = "src/main/resources/state-list.json";
+    private HashMap<String,String> stateList;
+
+    public HashMap<String,String> getStateList(){
+        return stateList;
+    }
+
+    @PostConstruct
+    public void fetchData() {
+        stateList = new HashMap<>();
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            stateList = mapper.readValue(new File(PATH), HashMap.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
